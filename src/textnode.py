da0c2b1c3bd527b0c57a3dd1c5b59d94068dcdd6
Oscar-Noonan <oscar.noonan13@gmail.com>
2026-09-08 17:41:52 +0100

    feat: adds function to convert text nodes to html

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -25,3 +27,19 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+    def text_node_to_html_node(self) -> LeafNode:
+        if self.text_type == TextType.TEXT:
+            return LeafNode(None, self.text)
+        elif self.text_type == TextType.BOLD:
+            return LeafNode("b", self.text)
+        elif self.text_type == TextType.ITALIC:
+            return LeafNode("i", self.text)
+        elif self.text_type == TextType.CODE:
+            return LeafNode("code", self.text)
+        elif self.text_type == TextType.LINK:
+            return LeafNode("a", self.text, {"href": self.url})
+        elif self.text_type == TextType.IMAGE:
+            return LeafNode("img", None, {"src": self.url, "alt": self.text})
+        else:
+            raise Exception
